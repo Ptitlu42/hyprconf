02679e7b38b1cf6aac907fa6e20b33720f1d9ee1
Ptitlu42 <lucas.beyer@gmx.fr>
2024-02-19 23:54:21 +0100

    Debug swaylock

---
+++ readme.md
@@ -0,0 +1,13 @@
+# Hyprland config
+
+Custom configuration for hyprLand. With a light theme and a dark theme.
+
+# Light
+
+![screen](screen1.png)
+![screen](screen2.png)
+
+# Dark
+
+![screen](screen3.png)
+![screen](screen4.png)
--- scripts/LockScreen.sh
+++ scripts/LockScreen.sh
@@ -2,4 +2,15 @@
 
 CONFIG="$HOME/.config/hypr/swaylock/config"
 
-sleep 0.5s; swaylock --config ${CONFIG} & disown
+screenshot=$(mktemp).png
+blurred=$(mktemp).png
+
+grim "$screenshot"
+
+convert "$screenshot" -blur 0x2 "$blurred"
+
+sleep 0.5s
+
+swaylock --config "${CONFIG}" -i "$blurred" & disown
+
+(sleep 15s && rm "$screenshot" "$blurred") & disown
