eae0a06ddcf4981f8e0e45853396d58852946a31
Ptitlu42 <lucas.beyer@gmx.fr>
2024-02-21 11:03:25 +0100

    Mouseless keybinds

--- scripts/Waybar.sh
+++ scripts/Waybar.sh
@@ -3,6 +3,8 @@
 CONFIG="$HOME/.config/hypr/waybar/config"
 STYLE="$HOME/.config/hypr/waybar/style.css"
 
-if [[ ! $(pidof waybar) ]]; then
-	waybar --bar main-bar --log-level error --config ${CONFIG} --style ${STYLE}
+if pidof waybar >/dev/null; then
+    killall waybar
+else
+    /usr/bin/waybar --config ${CONFIG} --style ${STYLE} &
 fi
